20c634a10ed87630304421f5d4ac94ee4f791015
Kevin McGarry <kevinjmcgarry@gmail.com>
2017-10-29 17:42:09 -0500

    added dependency and updated app

--- hello.js
+++ hello.js
@@ -1,4 +1,15 @@
 #!/usr/bin/env node
 
+var express = require('express');
+var app = express();
 
-console.log('Modern software projects start here.')
+app.get('/', function (req, res) {
+  res.send('Modern software projects start here.');
+});
+
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Modern software projects start here. http://%s:%s', host, port);
+});
